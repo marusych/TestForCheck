4f893f0c40b850e3621604166b96dfbe7399b6ba
Marusych Alex <marusich.alex@nixsolutions.com>
2018-07-24 19:12:44 +0300

    add commit first

--- src/Practice5/Task3.java
+++ src/Practice5/Task3.java
@@ -4,21 +4,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Task3 {
     public static void main(String[] args) throws IOException {
         ArrayList<Integer> test = new ArrayList();
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 3; i++) {
             int first = Integer.parseInt(reader.readLine());
             test.add(first);
         }
 
-        for (int i = 0; i < test.size(); i++) {
-            if (test.get(i)>test.get(i+1)){
-
-            }
-        }
+        test.sort(Integer::compareTo);
+        System.out.println(test.toString());
+        //System.out.println(Arrays.toString(test));
 
     }
 }
